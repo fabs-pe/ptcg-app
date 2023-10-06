99b9582b0cd76bd61f0dc5614de12b83748b970e
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-10-07 08:47:29 +1000

    feat: complete fetch

--- src/contexts/pages/CardSearchByName.jsx
+++ src/contexts/pages/CardSearchByName.jsx
@@ -1,43 +1,53 @@
-import { useContext, useEffect, useState } from "react";
+
+import { useContext, useEffect, useState } from "react"
 import { ApiContext } from "../ApiContext";
 import { useParams } from "react-router-dom";
 
 
-export default function CardSearchByName(){
+export default function CardSearchByName() {
 
-    // search results
+	// search results 
+	const [searchResults, setSearchResults] = useState([]);
 
-    const  [searchResults, setSearchResults] = useState();
+	// api URL 
+	const {api} = useContext(ApiContext);
 
-    //api url
-    const {api} = useContext(ApiContext)
+	// route param for the pokemon name 
+	const {pokemonName} = useParams();
 
-    //route param for name
-    const {pokemonName} = useParams();
+	// api key 
+	let apiKey = "6189f660-5c8b-489a-ad60-a7ad36f35819";
 
-    //api key
-    let apiKey = "6189f660-5c8b-489a-ad60-a7ad36f35819";
+	useEffect(() => {
+		console.log("Card search component has mounted! Making a fetch request now...");
 
-    useEffect(() => {
-        console.log("Card search component has mounted. Making a fetch request now");
+		async function apiRequest(){
+			let queryParams = new URLSearchParams({
+				q: 'name:' + pokemonName
+			})
+			let response = await fetch(api + 'cards?' + queryParams, {
+				headers: {
+					'X-Api-Key': apiKey
+				}
+			});
 
-        async function apiRequest(){
-            let queryParams = new URLSearchParams({
-                q: 'name'+pokemonName
-            })
-            let response = await fetch(api + 'cards?' + queryParams, {
-                headers:{
-                    'X-Api-Key' : apiKey
-                }
-            })
-        }
+			let responseData = await response.json();
 
+			setSearchResults(responseData.data);
+		}
 
-    }, {})
+		apiRequest();
 
-    return(
-        <div>
-            <h1>Card Search</h1>
-        </div>
-    )
+	}, []);
+
+	return (
+		<div>
+			<h1>Card Search</h1>
+			{searchResults.length > 0 && 
+			<div>
+				<h1>{searchResults[0].name} - {searchResults[0].id}</h1>
+			</div>
+			}
+		</div>
+	)
 }
